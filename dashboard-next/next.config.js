a445e4be11218ad26e71f4806bfba60f9b17391f
akshaymore1819 <akshaymore.intellisys@gmail.com>
2026-03-13 23:39:34 +0530

    Add webpack aliases for lib and models

--- dashboard-next/next.config.js
+++ dashboard-next/next.config.js
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   experimental: {
     serverActions: {
       allowedOrigins: ['localhost:3000']
     }
+  },
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@/lib': path.resolve(__dirname, 'lib'),
+      '@/models': path.resolve(__dirname, 'models')
+    };
+    return config;
   }
 };
 
