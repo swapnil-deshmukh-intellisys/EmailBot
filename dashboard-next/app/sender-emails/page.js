c9b54057d133ed9f287fbe3570eff199422db12a
akshay-intellisys <akshaymore.intellisys@gmail.com>
2026-05-04 12:12:58 +0530

    render problem solve 2

--- dashboard-next/app/sender-emails/page.js
+++ dashboard-next/app/sender-emails/page.js
@@ -1,5 +1,7 @@
 import { WorkspaceSectionPage, workspacePageConfigs } from '@/shared-components/common-components/workspace-components/WorkspaceComponentExports';
 
+export const dynamic = 'force-dynamic';
+
 export default function SenderEmailsPage() {
   return <WorkspaceSectionPage {...workspacePageConfigs['sender-emails']} />;
 }
